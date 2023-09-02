c7b02619b7ee523fa297ec374d66ba6a36b48b13
Camilo Salinas <camilon9035@hotmail.com>
2023-09-02 02:01:29 -0500

    Refactoring

--- testbuddy/src/providers/testlist/testListProvider.ts
+++ testbuddy/src/providers/testlist/testListProvider.ts
@@ -1,15 +1,11 @@
 import * as vscode from "vscode";
 
 import { getTestListHtml } from "./testListHtml";
-import { execShell, execShellMultiplatform } from "../../utils/execShell";
-import { checkNpm } from "../../utils/checkEnv";
-import { parse } from "jest-editor-support";
 import { ParsedNode } from "jest-editor-support/index";
 import * as fs from "fs";
 import { generateTests } from "../../utils/useAxios";
-import { Blob } from "buffer";
-import { ISuccessResponse } from "../../types/response";
 import path = require("path");
+import { loadCurrentTests, loadScripts } from "../../commands/commands";
 
 //https://stackoverflow.com/questions/43007267/how-to-run-a-system-command-from-vscode-extension Check answers at the end, fs.watch for file updates
 export class TestListWebViewViewProvider implements vscode.WebviewViewProvider {
@@ -46,21 +42,6 @@ export class TestListWebViewViewProvider implements vscode.WebviewViewProvider {
 
     webviewView.webview.onDidReceiveMessage(async (data) => {
       switch (data.type) {
-        case "colorSelected": {
-          vscode.workspace.fs
-            .writeFile(
-              vscode.Uri.joinPath(
-                vscode.workspace.workspaceFolders[0].uri,
-                "results/test" + data.value + ".txt"
-              ),
-              data.value
-            )
-            .then(console.log);
-          vscode.window.activeTextEditor?.insertSnippet(
-            new vscode.SnippetString(`#${data.value}`)
-          );
-          break;
-        }
         case "runTest": {
           let fileURL = data.value;
           console.log(fileURL);
@@ -94,57 +75,16 @@ export class TestListWebViewViewProvider implements vscode.WebviewViewProvider {
 
   async test(testUrl: string, itBlock?: string) {}
 
-  async loadScripts() {
-    let tesBuddyFolder = await execShellMultiplatform(
-      `cd ${vscode.workspace.workspaceFolders[0].uri.fsPath} && mkdir -p testbuddy`,
-      `cd ${vscode.workspace.workspaceFolders[0].uri.fsPath} && if not exist "testbuddy" mkdir testbuddy`
-    );
-    //TO-DO: Add testbuddy folder to gitignore, Define gitignore rules, add sesion id for teams/workspaces as a file. Create configuration file like .prettierrc(?)
-    let testbuddyCMD = await execShell(
-      `cd ${vscode.workspace.workspaceFolders[0].uri.fsPath} && npm pkg set "scripts.testbuddy"="jest --json --outputFile=./testbuddy/output.json"`
-    );
-    console.log("testBuddy");
-    let testbuddyListCMD = await execShell(
-      `cd ${vscode.workspace.workspaceFolders[0].uri.fsPath} && npm pkg set "scripts.testbuddy:list"="jest --listTests --json > ./testbuddy/testList.json"`
-    );
-    console.log("testBuddyList");
-  }
-  async loadCurrentTests(): Promise<ParsedNode[]> {
-    console.log("load Current");
-
-    await execShell(
-      `cd ${vscode.workspace.workspaceFolders[0].uri.fsPath} && npm run testbuddy:list`
-    );
-    console.log("Run List");
-    let testsList: string[] = JSON.parse(
-      fs.readFileSync(
-        vscode.Uri.joinPath(
-          vscode.workspace.workspaceFolders[0].uri,
-          "testbuddy/testList.json"
-        ).fsPath,
-        "utf8"
-      )
-    );
-
-    let results = testsList.map((el) => {
-      let parsedTests = parse(el);
-      let root = parsedTests.root;
-      root.children = parsedTests.itBlocks;
-      return root;
-    });
-
-    return results;
-  }
   public addTests(testList: ParsedNode[]) {
     console.log(testList);
     this._view?.webview.postMessage({ type: "addTests", content: testList });
   }
 
   public async initialize() {
-    return this.loadScripts()
+    return loadScripts()
       .then(() => {
         console.log("LOADED");
-        return this.loadCurrentTests();
+        return loadCurrentTests();
       })
       .then((tests) => {
         console.log("TESTS", tests);
@@ -184,19 +124,6 @@ export class TestListWebViewViewProvider implements vscode.WebviewViewProvider {
         error,
         "Oops an error has happened. Please try again"
       );
-    }
-  }
-
-  public addColor() {
-    if (this._view) {
-      this._view.show?.(true); // `show` is not implemented in 1.49 but is for 1.50 insiders
-      this._view.webview.postMessage({ type: "addColor" });
-    }
-  }
-
-  public clearColors() {
-    if (this._view) {
-      this._view.webview.postMessage({ type: "clearColors" });
     }
   }
 }
