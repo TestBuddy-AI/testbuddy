fc8c6112061624f59679054e72f612ebddd4ac01
Daniel Reyes <df.reyesr@uniandes.edu.co>
2023-09-11 23:07:06 -0500

    Trained model to modify endpoint
    Trained the model to handle modifySingleUnitTest endpoints

--- server/services/openaiService.ts
+++ server/services/openaiService.ts
@@ -104,7 +104,7 @@ export async function regenerateSingleUnitTest(
       {
         role: "system",
         content:
-          "Your function is to identify and remove a block of it() code and then replace it for another it() test that is neither is the one you removed and is not contained in the set of unit tests. This will be your algorithm: 1. Input: <unitTests> String with set of unit tests. <testToChange>: A string with the description of the test to replace from <UnitTests> with a new generated one. <function> which the unit tests where generated for. 2. You remove the corresponding <testToChange> from <UnitTests>, generate an unique new unit tests(it was not the one you removed and is not contained in the set of unit tests) and then you add it to the set of unit tests. 3. Output: The resulting code with the new unit tests replacing the requested one. CONSTRAINTS: 1. Your code should be exactly executable code but without the removed test. 2. DO NOT include any natural language or explanation. 3. The code must be ready to be executed from an endpoint. 4. The replacing test can't be any similar to the replaced one.",
+          "Your function is to identify and remove a block of it() code and then replace it for another it() test that neither is the one you removed and is not contained in the set of unit tests. This will be your algorithm: 1. Input: <unitTests> String with set of unit tests. <testToChange>: A string with the description of the test to replace from <UnitTests> with a new generated one. <function> which the unit tests where generated for. 2. You remove the corresponding <testToChange> from <UnitTests>, generate an unique new unit tests(it was not the one you removed and is not contained in the set of unit tests) and then you add it to the set of unit tests. 3. Output: The resulting code with the new unit tests replacing the requested one. CONSTRAINTS: 1. Your code should be exactly executable code. 2. DO NOT include any natural language or explanation. 3. The code must be ready to be executed from an endpoint. 4. The replacing test can't be any similar to the replaced one.",
       },
       {
         role: "user",
@@ -439,7 +439,9 @@ export async function modifySingleFunctionUnitTest(
     userInput
   );
 
-  console.log(`⚡️Given the test to change ${testToChange} with the input ${userInput} ChatGPT returns`);
+  console.log(
+    `⚡️Given the test to change ${testToChange} with the input ${userInput} ChatGPT returns`
+  );
   console.log(choices[0].message?.content);
   return choices[0].message?.content;
 }
@@ -450,88 +452,17 @@ export async function modifySingleUnitTest(
   unitTestToChange: string,
   userInput: string
 ): Promise<CreateChatCompletionResponse> {
-  // TODO: mi amor usa el input para modificar el method plis
-  console.log(userInput); // Quitarlo
   const response = await openAI.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: [
       {
         role: "system",
         content:
-          "Your function is to identify and remove a block of it() code and then replace it for another it() test that is neither is the one you removed and is not contained in the set of unit tests. This will be your algorithm: 1. Input: <unitTests> String with set of unit tests. <testToChange>: A string with the description of the test to replace from <UnitTests> with a new generated one. <function> which the unit tests where generated for. 2. You remove the corresponding <testToChange> from <UnitTests>, generate an unique new unit tests(it was not the one you removed and is not contained in the set of unit tests) and then you add it to the set of unit tests. 3. Output: The resulting code with the new unit tests replacing the requested one. CONSTRAINTS: 1. Your code should be exactly executable code but without the removed test. 2. DO NOT include any natural language or explanation. 3. The code must be ready to be executed from an endpoint. 4. The replacing test can't be any similar to the replaced one.",
+          "Your function is to identify and remove a block of it() code and then replace it for another it() test that neither is the one you removed and is not contained in the set of unit tests. This will be your algorithm: 1. Input: <unitTests> String with set of unit tests. <userInput> the user's modifications request to be change no the specified testToChange..<testToChange>: A string with the description of the test to be modified according to the userInput requests. <function> which the unit tests where generated for (this is just for context purposes). 2. You modify the corresponding <testToChange> from <UnitTests> with the modifications requested from the <userInput>. 3. Output: The resulting code with the unit tests suite and the modified unit tests. CONSTRAINTS: 1. Your code should be exactly executable code. 2. DO NOT include any natural language or explanation. 3. The code must be ready to be executed from an endpoint. 4. The replacing test can't be any similar to the replaced one.",
       },
       {
         role: "user",
-        content: "Starting replacing process.",
-      },
-      {
-        role: "assistant",
-        content: "Input: unitTests",
-      },
-      {
-        role: "user",
-        content:
-          "// Test case for typical scenario\nit('should return the sum of two numbers', () => {\n  expect(sum(2, 3)).toBe(5);\n});\n\n// Test case for negative numbers\nit('should return the sum of two negative numbers', () => {\n  expect(sum(-2, -3)).toBe(-5);\n});\n\n// Test case for zero\nit('should return the sum of zero and a number', () => {\n  expect(sum(0, 5)).toBe(5);\n});\n\n// Test case for decimal numbers\nit('should return the sum of two decimal numbers', () => {\n  expect(sum(1.5, 2.5)).toBe(4);\n});\n\n// Test case for large numbers\nit('should return the sum of two large numbers', () => {\n  expect(sum(1000000000, 2000000000)).toBe(3000000000);\n});",
-      },
-      {
-        role: "assistant",
-        content: "Input: testToChange",
-      },
-      {
-        role: "user",
-        content: "should return the sum of two numbers",
-      },
-      {
-        role: "assistant",
-        content: "Input: function",
-      },
-      {
-        role: "user",
-        content: "function sum(a, b) { return a + b; }",
-      },
-      {
-        role: "assistant",
-        content:
-          "// Test case for negative numbers\nit('should return the sum of two negative numbers', () => {\n  expect(sum(-2, -3)).toBe(-5);\n});\n\n// Test case for zero\nit('should return the sum of zero and a number', () => {\n  expect(sum(0, 5)).toBe(5);\n});\n\n// Test case for decimal numbers\nit('should return the sum of two decimal numbers', () => {\n  expect(sum(1.5, 2.5)).toBe(4);\n});\n\n// Test case for large numbers\nit('should return the sum of two large numbers', () => {\n  expect(sum(1000000000, 2000000000)).toBe(3000000000);\n});\n\n// Test case for typical scenario\nit('should return the sum of two numbers', () => {\n  expect(sum(8, 10)).toBe(18);\n});",
-      },
-      {
-        role: "user",
-        content: "Starting replacing process.",
-      },
-      {
-        role: "assistant",
-        content: "Input: unitTests",
-      },
-      {
-        role: "user",
-        content:
-          "// Test case for typical scenario\nit('should calculate mileage for given distance and fuel', () => {\n  const mileage = calculateMileage(100, 5);\n  expect(mileage).toBe(20);\n});\n\n// Test case for zero distance\nit('should return 0 mileage for 0 distance traveled', () => {\n  const mileage = calculateMileage(0, 5);\n  expect(mileage).toBe(0);\n});\n\n// Test case for decimal values\nit('should handle decimal values correctly', () => {\n  const mileage = calculateMileage(150.5, 7.5);\n  expect(mileage).toBeCloseTo(20.0667, 4); // 4 decimal places\n});\n\n// Test case for negative fuel (which shouldn't be allowed)\nit('should throw an error for negative fuel values', () => {\n  expect(() => {\n    calculateMileage(100, -5);\n  }).toThrow(\"Fuel consumed must be greater than 0\");\n});\n\n// Test case for zero fuel (which shouldn't be allowed)\nit('should throw an error for zero fuel', () => {\n  expect(() => {\n    calculateMileage(100, 0);\n  }).toThrow(\"Fuel consumed must be greater than 0\");\n});",
-      },
-      {
-        role: "assistant",
-        content: "Input: testToChange",
-      },
-      {
-        role: "user",
-        content: "should throw an error for negative fuel values",
-      },
-      {
-        role: "assistant",
-        content: "Input: function",
-      },
-      {
-        role: "user",
-        content:
-          'function calculateMileage(distance, fuel) {\n  if (fuel <= 0) {\n    throw new Error("Fuel consumed must be greater than 0");\n  }\n  return distance / fuel;\n}',
-      },
-      {
-        role: "assistant",
-        content:
-          "// Test case for typical scenario\nit('should calculate mileage for given distance and fuel', () => {\n  const mileage = calculateMileage(100, 5);\n  expect(mileage).toBe(20);\n});\n\n// Test case for zero distance\nit('should return 0 mileage for 0 distance traveled', () => {\n  const mileage = calculateMileage(0, 5);\n  expect(mileage).toBe(0);\n});\n\n// Test case for decimal values\nit('should handle decimal values correctly', () => {\n  const mileage = calculateMileage(150.5, 7.5);\n  expect(mileage).toBeCloseTo(20.0667, 4); // 4 decimal places\n});\n\n// Test case for zero fuel (which shouldn't be allowed)\nit('should throw an error for zero fuel', () => {\n  expect(() => {\n    calculateMileage(100, 0);\n  }).toThrow(\"Fuel consumed must be greater than 0\");\n});\n\n// Test case for negative distance (which shouldn't be allowed)\nit('should throw an error for negative distance values', () => {\n  expect(() => {\n    calculateMileage(-100, 5);\n  }).toThrow(\"Distance traveled must be non-negative\");\n});",
-      },
-      {
-        role: "user",
-        content: "Starting replacing process.",
+        content: "Starting replacing based on user input process.",
       },
       {
         role: "assistant",
@@ -560,12 +491,107 @@ export async function modifySingleUnitTest(
       },
       {
         role: "assistant",
-        content:
-          "// Test case for negative numbers\nit('should return the sum of two negative numbers', () => {\n  expect(sum(-2, -3)).toBe(-5);\n});\n\n// Test case for zero\nit('should return the sum of zero and a number', () => {\n  expect(sum(0, 5)).toBe(5);\n});\n\n// Test case for large numbers\nit('should return the sum of two large numbers', () => {\n  expect(sum(1000000000, 2000000000)).toBe(3000000000);\n});\n\n// Test case for typical scenario\nit('should return the sum of two numbers', () => {\n  expect(sum(2, 7)).toBe(9);\n});",
+        content: "Input: userInput",
       },
       {
         role: "user",
-        content: "Start replacing process.",
+        content: "Try with numbers that are bigger than 10",
+      },
+      {
+        role: "assistant",
+        content:
+          "// Test case for typical scenario\nit('should return the sum of two numbers', () => {\n  expect(sum(2, 3)).toBe(5);\n});\n\n// Test case for negative numbers\nit('should return the sum of two negative numbers', () => {\n  expect(sum(-2, -3)).toBe(-5);\n});\n\n// Test case for zero\nit('should return the sum of zero and a number', () => {\n  expect(sum(0, 5)).toBe(5);\n});\n\n// Test case for decimal numbers\nit('should return the sum of two numbers bigger than 10', () => {\n  expect(sum(12.5, 15.7)).toBe(28.2);\n});\n\n// Test case for large numbers\nit('should return the sum of two large numbers', () => {\n  expect(sum(1000000000, 2000000000)).toBe(3000000000);\n});",
+      },
+      {
+        role: "user",
+        content: "Starting replacing based on user input process.",
+      },
+      {
+        role: "assistant",
+        content: "Input: unitTests",
+      },
+      {
+        role: "user",
+        content:
+          "// Test case for typical scenario\nit('should calculate mileage for given distance and fuel', () => {\n  const mileage = calculateMileage(100, 5);\n  expect(mileage).toBe(20);\n});\n\n// Test case for zero distance\nit('should return 0 mileage for 0 distance traveled', () => {\n  const mileage = calculateMileage(0, 5);\n  expect(mileage).toBe(0);\n});\n\n// Test case for decimal values\nit('should handle decimal values correctly', () => {\n  const mileage = calculateMileage(150.5, 7.5);\n  expect(mileage).toBeCloseTo(20.0667, 4); // 4 decimal places\n});\n\n// Test case for negative fuel (which shouldn't be allowed)\nit('should throw an error for negative fuel values', () => {\n  expect(() => {\n    calculateMileage(100, -5);\n  }).toThrow(\"Fuel consumed must be greater than 0\");\n});\n\n// Test case for zero fuel (which shouldn't be allowed)\nit('should throw an error for zero fuel', () => {\n  expect(() => {\n    calculateMileage(100, 0);\n  }).toThrow(\"Fuel consumed must be greater than 0\");\n});",
+      },
+      {
+        role: "assistant",
+        content: "Input: testToChange",
+      },
+      {
+        role: "user",
+        content: "should calculate mileage for given distance and fuel",
+      },
+      {
+        role: "assistant",
+        content: "Input: function",
+      },
+      {
+        role: "user",
+        content:
+          'function calculateMileage(distance, fuel) {\n  if (fuel <= 0) {\n    throw new Error("Fuel consumed must be greater than 0");\n  }\n  return distance / fuel;\n}',
+      },
+      {
+        role: "assistant",
+        content: "Input: userInput",
+      },
+      {
+        role: "user",
+        content:
+          "Try to fit the unit test into a city distances, 100 miles is too wide",
+      },
+      {
+        role: "assistant",
+        content:
+          "// Test case for typical scenario\nit('should calculate mileage for given distance and fuel', () => {\n  const mileage = calculateMileage(50, 2);\n  expect(mileage).toBe(25);\n});\n\n// Test case for zero distance\nit('should return 0 mileage for 0 distance traveled', () => {\n  const mileage = calculateMileage(0, 2);\n  expect(mileage).toBe(0);\n});\n\n// Test case for decimal values\nit('should handle decimal values correctly', () => {\n  const mileage = calculateMileage(150.5, 7.5);\n  expect(mileage).toBeCloseTo(20.0667, 4); // 4 decimal places\n});\n\n// Test case for negative fuel (which shouldn't be allowed)\nit('should throw an error for negative fuel values', () => {\n  expect(() => {\n    calculateMileage(100, -5);\n  }).toThrow(\"Fuel consumed must be greater than 0\");\n});\n\n// Test case for zero fuel (which shouldn't be allowed)\nit('should throw an error for zero fuel', () => {\n  expect(() => {\n    calculateMileage(100, 0);\n  }).toThrow(\"Fuel consumed must be greater than 0\");\n});",
+      },
+      {
+        role: "user",
+        content: "Starting replacing based on user input process.",
+      },
+      {
+        role: "assistant",
+        content: "Input: unitTests",
+      },
+      {
+        role: "user",
+        content:
+          "// Test case for typical scenario without discount\nit('should calculate total price for items without discount', () => {\n  const cart = [\n    { price: 50, quantity: 2 },\n    { price: 100, quantity: 1 }\n  ];\n  const total = calculateTotalPrice(cart, \"\");\n  expect(total).toBe(200);\n});\n\n// Test case for SALE10 discount\nit('should apply 10% discount for SALE10 code', () => {\n  const cart = [\n    { price: 100, quantity: 2 }\n  ];\n  const total = calculateTotalPrice(cart, \"SALE10\");\n  expect(total).toBe(180); // 200 - 10% = 180\n});\n\n// Test case for SALE20 discount\nit('should apply 20% discount for SALE20 code', () => {\n  const cart = [\n    { price: 100, quantity: 3 }\n  ];\n  const total = calculateTotalPrice(cart, \"SALE20\");\n  expect(total).toBe(240); // 300 - 20% = 240\n});\n\n// Test case for invalid discount code\nit('should not apply discount for invalid codes', () => {\n  const cart = [\n    { price: 100, quantity: 2 }\n  ];\n  const total = calculateTotalPrice(cart, \"INVALIDCODE\");\n  expect(total).toBe(200);\n});\n\n// Test case for empty cart\nit('should return 0 for an empty cart', () => {\n  const cart = [];\n  const total = calculateTotalPrice(cart, \"SALE10\");\n  expect(total).toBe(0);\n});",
+      },
+      {
+        role: "assistant",
+        content: "Input: testToChange",
+      },
+      {
+        role: "user",
+        content: "should calculate total price for items without discount",
+      },
+      {
+        role: "assistant",
+        content: "Input: function",
+      },
+      {
+        role: "user",
+        content:
+          'function calculateTotalPrice(cartItems, discountCode) {\n  let total = 0;\n\n  for (let item of cartItems) {\n    total += item.price * item.quantity;\n  }\n\n  if (discountCode === "SALE10") {\n    total *= 0.9; // 10% discount\n  } else if (discountCode === "SALE20") {\n    total *= 0.8; // 20% discount\n  }\n\n  return total;\n}',
+      },
+      {
+        role: "assistant",
+        content: "Input: userInput",
+      },
+      {
+        role: "user",
+        content: "Add more items to the test",
+      },
+      {
+        role: "assistant",
+        content:
+          "// Test case for typical scenario without discount\nit('should calculate total price for items without discount', () => {\n  const cart = [\n    { price: 50, quantity: 2 },\n    { price: 100, quantity: 1 },\n    { price: 25, quantity: 3 }\n  ];\n  const total = calculateTotalPrice(cart, \"\");\n  expect(total).toBe(250);\n});\n\n// Test case for SALE10 discount\nit('should apply 10% discount for SALE10 code', () => {\n  const cart = [\n    { price: 100, quantity: 2 },\n    { price: 50, quantity: 1 }\n  ];\n  const total = calculateTotalPrice(cart, \"SALE10\");\n  expect(total).toBe(360); // 400 - 10% = 360\n});\n\n// Test case for SALE20 discount\nit('should apply 20% discount for SALE20 code', () => {\n  const cart = [\n    { price: 100, quantity: 3 },\n    { price: 75, quantity: 2 }\n  ];\n  const total = calculateTotalPrice(cart, \"SALE20\");\n  expect(total).toBe(480); // 600 - 20% = 480\n});\n\n// Test case for invalid discount code\nit('should not apply discount for invalid codes', () => {\n  const cart = [\n    { price: 100, quantity: 2 },\n    { price: 50, quantity: 1 }\n  ];\n  const total = calculateTotalPrice(cart, \"INVALIDCODE\");\n  expect(total).toBe(250);\n});\n\n// Test case for empty cart\nit('should return 0 for an empty cart', () => {\n  const cart = [];\n  const total = calculateTotalPrice(cart, \"SALE10\");\n  expect(total).toBe(0);\n});",
+      },
+      {
+        role: "user",
+        content: "Starting replacing based on user input process.",
       },
       {
         role: "assistant",
@@ -591,9 +617,17 @@ export async function modifySingleUnitTest(
         role: "user",
         content: `${originalCode}`,
       },
+      {
+        role: "assistant",
+        content: "Input: userInput",
+      },
+      {
+        role: "user",
+        content: `${userInput}`,
+      },
     ],
     temperature: 1.2,
-    max_tokens: 1500,
+    max_tokens: 1000,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
